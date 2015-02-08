85da25c0b78b2a42092ba35f215be33e10adc2e9
ocheminat <ocheminat@gmail.com>
2015-02-08 20:41:24 +0100

    Revert "Plotting assignment 1: final modifications"
    This reverts commit d5e7be071caacd0f5bfce2b2b584cb81c68da74b.

--- plot3.R
+++ plot3.R
@@ -19,7 +19,7 @@ library(datasets)
 with (b, plot(y, Sub_metering_1, type="l", col = "black", xlab = "", ylab = "Energy sub metering"))
 with (b, lines(y, Sub_metering_2, type="l", col = "red"))
 with (b, lines(y, Sub_metering_3, type="l", col = "blue"))
-legend("topright", lwd=c(1,1,1), col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", lwd=c(2.5,2.5, 2.5), col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 ## IMAGE CREATION
 dev.copy(png, file = "plot3.png")
